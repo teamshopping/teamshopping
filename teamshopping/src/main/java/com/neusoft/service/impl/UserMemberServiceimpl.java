fc212fe350b9ad9d775c1c8cf7ad74868e6b863d
xiaohe <362243267@qq.com>
2018-08-23 11:33:11 +0800

    小河

--- teamshopping/src/main/java/com/neusoft/service/impl/UserMemberServiceimpl.java
+++ teamshopping/src/main/java/com/neusoft/service/impl/UserMemberServiceimpl.java
@@ -1,6 +1,8 @@
 package com.neusoft.service.impl;
 
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -51,6 +53,16 @@ public class UserMemberServiceimpl implements UserMemberService{
 		UserMember usermember= userMemberMapper.login(map);
 		return usermember;
 	}
-	
+	/**
+	 * md5º”√‹
+	 * @throws NoSuchAlgorithmException 
+	 */
+	public String getPwd(String pwd ) throws Exception{
+			MessageDigest md =MessageDigest.getInstance("MD5");
+			byte[] digest=md.digest(pwd.getBytes());
+			String newPwd=new String(digest);
+			return newPwd;
+			
+	}
 
 }
